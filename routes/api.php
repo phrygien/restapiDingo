0238f7a5efd6e967a2f55651e262a56cd7ce8c52
Mecene Phrygien <mecenephrygien@gmail.com>
2022-12-09 06:47:42 +0300

    setup projet avec Dingo ecosystem et jwt

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+    
+    $api->get('hello', function(){
+        return 'Hello DingoApi';
+    });
+});
