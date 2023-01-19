9b7dce81bd6388ea5b09dc22b7b9e665ee5492a3
Mecene Phrygien <mecenephrygien@gmail.com>
2023-01-19 04:34:57 +0300

    user activity log

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Haruncpi\LaravelUserActivity\Traits\Loggable;
 
 class Employee extends Model
 {
-    use HasFactory;
+    use Loggable, HasFactory;
 
     protected $table = 'employees';
 
